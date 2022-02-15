1d433924f2925fa62aa936998876e755bcf93cdd
Alma <al02974206@tecmilenio.mx>
2022-02-14 21:27:46 -0500

    Se agregaron comentarios

--- src/Actividad2.java
+++ src/Actividad2.java
@@ -1,14 +1,18 @@
 import java.util.*;
+//Autor: Alma Dimas Sanchez
 
 public class Actividad2{
-
+    //Metodo main
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        //declaración de variables
         String nombre;
         char letra='A';
         int[] calificaciones = new int[5];
         double totalpromedio = 0;
 
+            //Se pide al usuario que ingrese su nombre y sus calificaciones
         System.out.println("Ingrese el nombre del estudiante por favor");
         nombre = sc.nextLine();
         System.out.println("Ingrese sus 5 calificaciones en un rango de 0/100");
@@ -22,7 +26,7 @@ public class Actividad2{
         imprimir(nombre, calificaciones, totalpromedio, letra);
 
     }
-
+        //Primer método donde se hace el promedio
     public static double promedio (int[] califArray){
         double prom=0;
         for (int num : califArray) {
@@ -32,7 +36,7 @@ public class Actividad2{
         return prom;
     }
 
-
+            //segundo promedio donde el promedio se convierte a una letra
     public static char convertir (double Numero){
         char prom='F';
         if (Numero >90) {
@@ -49,7 +53,7 @@ public class Actividad2{
         }
         return prom;
     }
-
+            //tercer método donde se imprime los resultados
     public static void imprimir (String nombre, int[] Calificaciones, double totalPromedio, char letra){
         System.out.println("Nombre del estudiante " +nombre);
         System.out.println("Calificación 1: " +Calificaciones [0]);
